b7f18346724b44b6a8e1867827ad3fa7879842d2
Aditya Vishwakarma <vishwakarmaaditya471@gmail.com>
2025-06-23 12:24:53 +0530

    Day 1 – Unity setup + dodge block prototype + roguelike movement planned

---
+++ Day01_StarterProject/Assets/BlockSpawner.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockSpawner : MonoBehaviour
+{
+    public GameObject blockPrefab;
+    public float spawnInterval = 1f;
+    public float spawnRangeX = 8f;
+    public float top = 10f;
+
+    void Start()
+    {
+        InvokeRepeating("SpawnBlock", 1f, spawnInterval);
+    }
+
+    void SpawnBlock()
+    {
+        float randomX = Random.Range(-spawnRangeX, spawnRangeX);
+        Vector2 spawnPosition = new Vector2(randomX, top); // top of screen
+        Instantiate(blockPrefab, spawnPosition, Quaternion.identity);
+    }
+}
